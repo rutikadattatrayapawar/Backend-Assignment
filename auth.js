fc83aa6f2eaad27c8acc337503297aefcf9973c8
Rutika Dattatraya Pawar <127298139+rutikadattatrayapawar@users.noreply.github.com>
2023-05-13 17:57:56 +0530

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const router = express.Router();
+
+router.post('/signup', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(409).json({ message: 'User already exists' });
+    }
+    const user = new User({ email, password });
+    await user.save();
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+    res.status(201).json({ token });
+  } catch (error) {
+    res.status(
---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI;
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
+  })
+  .catch(error => console.log(error));
